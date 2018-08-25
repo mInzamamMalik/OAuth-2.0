1002f4da9790b8e4d13847fd4c6c8b79d24bb570
Mohammad Inzamam Malik <malikasinger1@gmail.com>
2018-08-25 16:52:46 +0500

    some more basic changes, OAuth is not functional yet

--- dist/app.js
+++ dist/app.js
@@ -1,22 +1,20 @@
-const OAuth2Server = require('oauth2-server');
-
-const oauth = new OAuth2Server({
+var OAuth2Server = require('oauth2-server');
+var oauth = new OAuth2Server({
     model: require('./model'),
     allowBearerTokensInQueryString: true,
     accessTokenLifetime: 4 * 60 * 60
 });
-
 function authenticateHandler(options) {
     return function (req, res, next) {
-        let request = new Request(req);
-        let response = new Response(res);
+        var request = new Request(req);
+        var response = new Response(res);
         return oauth.authenticate(request, response, options)
             .then(function (token) {
-                res.locals.oauth = { token: token };
-                next();
-            })
+            res.locals.oauth = { token: token };
+            next();
+        })
             .catch(function (err) {
-                // handle error condition
-            });
-    }
+            // handle error condition
+        });
+    };
 }
